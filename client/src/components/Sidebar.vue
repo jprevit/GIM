7aa391b3365e38e79ab5b585110fadf2df2a4475
John Previtera <johngprevitera@gmail.com>
2024-11-22 14:56:18 -0700

    adjusted breakpoint for sidebar text

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -4,26 +4,27 @@
 
 
 <template>
-    <div class="col-2 bg-secondary text-light fs-5 p-0 d-flex flex-column justify-content-around height textshadow">
+    <div
+        class="col-2 col-sm-1 bg-secondary text-light fs-5 p-0 d-flex flex-column justify-content-around height textshadow">
         <router-link class="justify-content-center align-content-center text-center navlink row me-0"
             :to="{ name: 'Receiving' }" title="Receiving">
             <i class="mdi mdi-truck col-8 fs-4"></i>
-            <p class="m-0 d-none d-md-flex col-8 justify-content-center">Receiving</p>
+            <p class="m-0 d-none d-lg-flex col-8 justify-content-center">Receiving</p>
         </router-link>
         <router-link class="justify-content-center align-content-center text-center navlink row me-0"
             :to="{ name: 'Inventory' }" title="Inventory">
             <i class="mdi mdi-treasure-chest col-8 fs-4"></i>
-            <p class="m-0 d-none d-md-flex col-8 justify-content-center">Inventory</p>
+            <p class="m-0 d-none d-lg-flex col-8 justify-content-center">Inventory</p>
         </router-link>
         <router-link class="justify-content-center align-content-center text-center navlink row me-0"
             :to="{ name: 'Production' }" title="Production">
             <i class="mdi mdi-hammer-screwdriver col-8 fs-4"></i>
-            <p class="m-0 d-none d-md-flex col-8 justify-content-center">Production</p>
+            <p class="m-0 d-none d-lg-flex col-8 justify-content-center">Production</p>
         </router-link>
         <router-link class="justify-content-center align-content-center text-center navlink row me-0"
             :to="{ name: 'Sales' }" title="Sales">
             <i class="mdi mdi-cash-multiple col-8 fs-4"></i>
-            <p class="m-0 d-none d-md-flex col-8 justify-content-center">Sales</p>
+            <p class="m-0 d-none d-lg-flex col-8 justify-content-center">Sales</p>
         </router-link>
     </div>
 </template>
